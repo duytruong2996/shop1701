dd51ede386dc9cabb5b3aeeb55fc225764c4f0dd
huong <huongnguyenak96@gmail.com>
2018-04-20 18:16:41 +0700

    select categiry

--- model/HomeModel.php
+++ model/HomeModel.php
@@ -7,7 +7,16 @@ class HomeModel extends DBConnect{
         $sql = "SELECT * FROM slide WHERE status=1";
         return $this->loadMoreRows($sql);
     }
-    
+    /**
+     * 
+     * 
+     *  SELECT menu.name, GROUP_CONCAT(sub.name) as submenu
+     *  FROM (SELECT * FROM `categories` WHERE id_parent IS NULL) menu
+     *  LEFT JOIN (SELECT * FROM `categories` WHERE id_parent IS NOT NULL) sub 
+     *      ON menu.id = sub.id_parent
+     *  GROUP BY menu.name
+     *  ORDER BY submenu DESC
+     */
 }
 
 
