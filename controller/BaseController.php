5aac5b14792374035a307c1a01af79b9a878b54b
huong <huongnguyenak96@gmail.com>
2018-04-20 19:16:37 +0700

    menu multi level

--- controller/BaseController.php
+++ controller/BaseController.php
@@ -1,8 +1,14 @@
 <?php
+include_once 'model/BaseModel.php';
 
 class BaseController {
 
     function loadView($view="home", $data=[]){ 
+
+        $model = new BaseModel;
+        $categories = $model->selectCategories();
+       // print_r($categories);
+       // die;
         include_once 'view/layout.view.php';
     }
 
--- model/BaseModel.php
+++ model/BaseModel.php
@@ -1,10 +1,25 @@
 <?php
 include_once 'DBConnect.php';
 
-class HomeModel extends DBConnect{
+class BaseModel extends DBConnect{
 
-    function selectSlide(){
-        $sql = "SELECT * FROM slide WHERE status=1";
+    function selectCategories(){
+        $sql = "SELECT url.url, menu.name, menu.icon,  GROUP_CONCAT(sub.name, '::' ,sub.url ) as submenu 
+        FROM (
+            SELECT * FROM `categories` WHERE id_parent IS NULL
+        ) menu 
+        LEFT JOIN ( 
+            SELECT c.*, url.url 
+            FROM `categories` c 
+            INNER JOIN page_url url ON url.id = c.id_url 
+            WHERE c.id_parent IS NOT NULL 
+        ) sub 
+        ON menu.id = sub.id_parent 
+        INNER JOIN page_url url 
+        ON url.id = menu.id_url 
+        GROUP BY menu.name 
+        ORDER BY submenu DESC";
+
         return $this->loadMoreRows($sql);
     }
 
